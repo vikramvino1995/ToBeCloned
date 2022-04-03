13665a2ce4934fb74b6546f2e508b3a9ff82110e
vikramvino1995 <vikramvino.1995@gmail.com>
2022-04-03 12:57:33 +0530

    Modified by Vignesh

--- src/org/clone/Cloning.java
+++ src/org/clone/Cloning.java
@@ -5,6 +5,11 @@ public class Cloning {
 	public static void main(String[] args) {
 		
 		System.out.println("Cloned");
-		
+		System.out.println("6");
+		System.out.println("7");
+		System.out.println("8");
+		System.out.println("9");
+		System.out.println("10");
+		System.out.println("11");
 	}
 }
